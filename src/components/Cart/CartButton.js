edc24b90a1877d729f0f0aa6a779be5e902caee3
Nhan Vo <niick7@gmail.com>
2024-12-09 14:51:25 -0600

    Remove dummy data and using redux store

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,19 @@
-import classes from './CartButton.module.css';
+import { useSelector, useDispatch } from "react-redux";
+
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
 
 const CartButton = (props) => {
+  const itemCount = useSelector((state) => state.cart.totalQuantity);
+  const dispatch = useDispatch();
+  function handleToogle() {
+    dispatch(uiActions.toogle());
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={handleToogle}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{itemCount}</span>
     </button>
   );
 };
